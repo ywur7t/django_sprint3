cc9fb7098fe7e800daf9b8fc519e2aa1a7621e30
Ywur7t(Martyn_Vlad) <m4rtyn.v@yandex.ru>
2024-12-06 13:56:44 +1000

    -

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,21 +1,28 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+
 
 User = get_user_model()
 
 
 class Post(models.Model):
 
-    title = models.CharField(verbose_name='Заголовок', max_length=256, blank=False)
+    title = models.CharField(verbose_name='Заголовок',
+                             max_length=256, blank=False)
     text = models.TextField(verbose_name='Текст', blank=False)
-    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',   help_text='Если установить дату и время в будущем — можно делать отложенные публикации.', auto_now_add=True, blank=False)
-    author = models.ForeignKey(User, verbose_name='Автор публикации', on_delete=models.CASCADE, related_name='posts', blank=False)
-    location = models.ForeignKey('Location', verbose_name='Местоположение', on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
-    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.SET_NULL, null=True, blank=False, related_name='posts')
-    is_published = models.BooleanField(verbose_name='Опубликовано',  help_text='Снимите галочку, чтобы скрыть публикацию.', default=True, blank=False)
-    created_at = models.DateTimeField(verbose_name='Добавлено', auto_now_add=True, blank=False)
+    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
+                                    help_text='Если установить дату и время в будущем — можно делать отложенные публикации.', auto_now_add=True, blank=False)
+    author = models.ForeignKey(User, verbose_name='Автор публикации',
+                               on_delete=models.CASCADE, related_name='posts', blank=False)
+    location = models.ForeignKey('Location', verbose_name='Местоположение',
+                                 on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
+    category = models.ForeignKey('Category', verbose_name='Категория',
+                                 on_delete=models.SET_NULL, null=True, blank=False, related_name='posts')
+    is_published = models.BooleanField(
+        verbose_name='Опубликовано',  help_text='Снимите галочку, чтобы скрыть публикацию.', default=True, blank=False)
+    created_at = models.DateTimeField(
+        verbose_name='Добавлено', auto_now_add=True, blank=False)
 
     def __str__(self):
         return self.title
@@ -25,10 +32,11 @@ class Post(models.Model):
         verbose_name_plural = 'Публикации'
 
 
-
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name="comments")
-    author = models.ForeignKey(User,  verbose_name='Автор публикации', on_delete=models.CASCADE,related_name="comments")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User,  verbose_name='Автор публикации',
+                               on_delete=models.CASCADE, related_name="comments")
     content = models.TextField()
     date_commented = models.DateTimeField(auto_now_add=True)
 
@@ -36,15 +44,17 @@ class Comment(models.Model):
         return f"Comment by {self.author} on {self.post}"
 
 
-
-
 class Category(models.Model):
 
-    title = models.CharField(verbose_name='Заголовок', max_length=256, blank=False)
+    title = models.CharField(verbose_name='Заголовок',
+                             max_length=256, blank=False)
     description = models.TextField(verbose_name='Описание', blank=False)
-    slug = models.SlugField(verbose_name='Идентификатор',  help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.', unique=True, blank=False)
-    is_published = models.BooleanField(verbose_name='Опубликовано',  help_text='Снимите галочку, чтобы скрыть публикацию.', default=True, blank=False)
-    created_at = models.DateTimeField(verbose_name='Добавлено', auto_now_add=True, blank=False)
+    slug = models.SlugField(verbose_name='Идентификатор',
+                            help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.', unique=True, blank=False)
+    is_published = models.BooleanField(
+        verbose_name='Опубликовано',  help_text='Снимите галочку, чтобы скрыть публикацию.', default=True, blank=False)
+    created_at = models.DateTimeField(
+        verbose_name='Добавлено', auto_now_add=True, blank=False)
 
     def __str__(self):
         return self.name
@@ -54,11 +64,13 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
 
-
 class Location(models.Model):
-    name = models.CharField(verbose_name='Название места', max_length=256, blank=False)
-    is_published = models.BooleanField(verbose_name='Опубликовано', help_text='Снимите галочку, чтобы скрыть публикацию.', default=True, blank=False)
-    created_at = models.DateTimeField(verbose_name='Добавлено', auto_now_add=True, blank=False)
+    name = models.CharField(verbose_name='Название места',
+                            max_length=256, blank=False)
+    is_published = models.BooleanField(
+        verbose_name='Опубликовано', help_text='Снимите галочку, чтобы скрыть публикацию.', default=True, blank=False)
+    created_at = models.DateTimeField(
+        verbose_name='Добавлено', auto_now_add=True, blank=False)
 
     def __str__(self):
         return self.name
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Category, Location
+from .models import Post, Category
 from django.utils.timezone import now
 
 
@@ -19,7 +19,9 @@ def index(request):
 
 def post_detail(request, id):
 
-    post = get_object_or_404(Post.objects.select_related('author', 'category', 'location'), id=id, is_published=True)
+    post = get_object_or_404(
+        Post.objects.select_related('author', 'category', 'location'),
+        id=id, is_published=True)
     context = {'post': post}
 
     return render(request, 'blog/detail.html', context)
@@ -27,13 +29,12 @@ def post_detail(request, id):
 
 def category_posts(request, category_slug):
 
-    category = get_object_or_404(Category.objects.filter(is_published=True), slug=category_slug)
-    posts = Post.objects.filter(category=category, is_published=True).order_by('-pub_date')
+    category = get_object_or_404(Category.objects.filter(
+        is_published=True), slug=category_slug)
+    posts = Post.objects.filter(
+        category=category, is_published=True).order_by('-pub_date')
     context = {
         'category': category,
         'posts': posts,
     }
-
-
-
     return render(request, 'blog/category.html', context)
