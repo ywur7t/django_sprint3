321b10f5ec719dd12f5b162bd6a47f5e055a8770
Ywur7t(Martyn_Vlad) <m4rtyn.v@yandex.ru>
2024-12-06 20:03:53 +1000

    -

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Category, Location
+from .models import Post, Category
 from django.utils import timezone
 from django.http import Http404
 
